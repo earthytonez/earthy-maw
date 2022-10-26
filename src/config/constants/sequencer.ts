620de643d6c8e0b87720529e5956f046dc201f87
M Gimenez-Peterson <mikkergp@modquiz.com>
2022-10-26 06:59:18 -0700

    Update Sequencer imports

--- src/config/constants/sequencer.ts
+++ src/config/constants/sequencer.ts
@@ -1,18 +1,17 @@
 interface IStringAnyHashMap {
-    [slug: string]: any;
-} 
+  [slug: string]: any;
+}
 
 export const TOML_FILES: IStringAnyHashMap = {
-    "One Two": require("../../stores/Sequencer/Definitions/one_two"),
-    "One Four": require("../../stores/Sequencer/Definitions/one_four"),
-    "Two Four": require("../../stores/Sequencer/Definitions/two_four"),
-    "Simple Arpeggiator": require("../../stores/Sequencer/Definitions/simple_arpeggiator"),
-    "Three Four": require("../../stores/Sequencer/Definitions/three_four"),
-    "Four On The Floor": require("../../stores/Sequencer/Definitions/four_on_the_floor"),
-    "Off Beat Four": require("../../stores/Sequencer/Definitions/off_beat_four"),
-    "HiHat": require("../../stores/Sequencer/Definitions/hihat"),
-    "House HiHat": require("../../stores/Sequencer/Definitions/house_hihat"),
-    "Simple Drone": require("../../stores/Sequencer/Definitions/simple_drone"),
-    "Random": require("../../stores/Sequencer/Definitions/random")
-}
-  
+  "One Two": require("../../stores/Sequencer/Definitions/one_two"),
+  "One Four": require("../../stores/Sequencer/Definitions/one_four"),
+  "Two Four": require("../../stores/Sequencer/Definitions/two_four"),
+  "Simple Arpeggiator": require("../../stores/Sequencer/Definitions/simple_arpeggiator"),
+  "Three Four": require("../../stores/Sequencer/Definitions/three_four"),
+  "Four On The Floor": require("../../stores/Sequencer/Definitions/four_on_the_floor"),
+  "Off Beat Four": require("../../stores/Sequencer/Definitions/off_beat_four"),
+  HiHat: require("../../stores/Sequencer/Definitions/hihat"),
+  "House HiHat": require("../../stores/Sequencer/Definitions/house_hihat"),
+  "Simple Drone": require("../../stores/Sequencer/Definitions/simple_drone"),
+  Random: require("../../stores/Sequencer/Definitions/random"),
+};
